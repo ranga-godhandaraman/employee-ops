07fb72057e82908011384024ce5a8fdbe8ba47c3
ranga-godhandaraman <rangabashyam22@gmail.com>
2022-08-25 23:01:53 +0530

    footer created

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
 import Header from './component/header/header.js';
+import Footer from './component/footer/footer.js';  
 
 function App() {
   return (
     <div className='app'>
       <Header></Header>
       <div className='content'>
-        <div>Employee list</div>
+       <div>Employee list</div>
         <div>Add Employee</div>
         <div>Edit Employee</div>
-      </div>
-      <div className='footer'>copyright rangabashyam@gmail.com</div>
+       </div>
+       <Footer/>
+      
     </div>
   )
 }
---
+++ src/component/footer/footer.js
@@ -0,0 +1,10 @@
+import './footer.css'
+
+function Footer() {
+    return (
+        <div className='footer'>
+            copyright rangabashyam@gmail.com
+        </div>
+    )
+}
+export default Footer;
