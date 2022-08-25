49d3a50fd8fc11681ba1df647576505d7cc6ea36
Haribabu Pasupathy <rangabashyam22@gmail.com>
2022-08-25 22:29:29 +0530

    header created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import Header from './component/header/header.js';
 
 function App() {
   return (
-    <div className="App">
-      Hello world
+    <div className='app'>
+      <Header></Header>
+      <div className='content'>
+        <div>Employee list</div>
+        <div>Add Employee</div>
+        <div>Edit Employee</div>
+      </div>
+      <div className='footer'>copyright rangabashyam@gmail.com</div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/component/header/header.js
@@ -0,0 +1,12 @@
+import './header.css';
+
+function Header() {
+    return (
+        <div className='header'>
+            <h2>Employee Manager</h2>
+            <h3>Home</h3>
+        </div>
+    )
+}
+
+export default Header;
