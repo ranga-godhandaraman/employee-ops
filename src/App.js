ecc4a089ede29fbbdedd7d807a136791b000928e
Haribabu Pasupathy <rangabashyam22@gmail.com>
2022-08-26 11:15:35 +0530

    Router added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
+import React from 'react';
 import './App.css';
 import Header from './component/header/header.js';
-import Footer from './component/footer/footer.js';  
+import Footer from './component/footer/footer.js';
 import List from './pages/employees/list';
+import { Routes, Route } from "react-router-dom";
+import Login from './pages/login/login';
 
 function App() {
   return (
     <div className='app'>
-      <Header></Header>
+      <Header />
       <div className='content'>
-        <List />       
-       </div>
-       <Footer/>
-      
+        <Routes>
+          <Route path="/" element={<List />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+      <Footer />
     </div>
   )
 }
--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,10 +1,10 @@
+import React  from 'react';
 import './header.css';
 
 function Header() {
     return (
         <div className='header'>
             <h2>Manage Employees</h2>
-            <h3>Home</h3>
         </div>
     )
 }
